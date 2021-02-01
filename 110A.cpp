b6d47f0ec9ee12e32a0326e89864640ac23bb9bd
Preet Viradiya <69113578+pinakin7@users.noreply.github.com>
2021-02-01 14:10:59 +0530

    Create 110A.cpp

---
+++ 110A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    string s;
+    cin>>s;
+    int c=0;
+    for(int i = 0;i<s.length();i++){
+        
+        if(s[i]=='4'|s[i]=='7'){
+            c+=1;
+        }
+    }
+    if(c==4 | c==7){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
