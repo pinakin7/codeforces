e21166b7660b724b785537a0d1d439037dac7f58
Preet Viradiya <69113578+pinakin7@users.noreply.github.com>
2021-02-02 10:18:10 +0530

    Create 344A.cpp

---
+++ 344A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int n,c=0;
+    cin>>n;
+    int a[n];
+    for(int i = 0;i<n;i++){
+        cin>>a[i];
+        if(a[i]!=a[i-1]){
+            c++;
+        }
+    }
+    cout<<c;
+    return 0;
+}
