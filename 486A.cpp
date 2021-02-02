bff2f551c7c6cc35bfd7166fc09bc4c0fb3ab885
Preet Viradiya <69113578+pinakin7@users.noreply.github.com>
2021-02-02 10:55:12 +0530

    Create 486A.cpp

---
+++ 486A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<n/2;
+    }else{
+        cout<<0-((n/2)+1);
+    }
+    return 0;
+}
