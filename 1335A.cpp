06d2b49a9a18a85e71bfe585ae1d904effb95e3c
Preet Viradiya <69113578+pinakin7@users.noreply.github.com>
2021-02-03 10:06:05 +0530

    Create 1335A.cpp

---
+++ 1335A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    int n,x;
+    cin>>n;
+    while(n--){
+        cin>>x;
+        cout<<((x+1)/2)-1<<endl;
+    }
+    return 0;
+}
