c8590e3db9e0a0e1ca7655634e941429f5f56e8a
Preet Viradiya <69113578+pinakin7@users.noreply.github.com>
2021-02-12 12:27:42 +0530

    Create 472A.cpp

---
+++ 472A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(x%2 == 0){
+        cout<<4<<" "<<a-4;
+    }
+    else{
+        cout<<9<<" "<<a-9;
+    }
+return 0;    
+}
